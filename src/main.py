db7d95f75f7cbc1c4171c94e61d1972c57e08ad4
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2024-04-18 21:40:26 -0400

    Modify main pipeline to use txt chord files instead of audio files; Add chord_to_number utility function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os
 import torch
 import torch.utils
-import torchaudio
 from math import ceil
 from torchvision.io import read_video
 from tqdm import tqdm
 from typing import Optional
 
 from model import ScenephonyModel, ScenephonyModelConfig
+from utils import chord_to_number
 
 
 def preproc_vid(video: torch.Tensor, sample_frames: int, new_H: int, new_W: int) -> torch.Tensor:
@@ -44,7 +44,7 @@ def train(
     """Train the Scenephony model.
 
     Args:
-        data_dir: Path to the training dataset.
+        data_dir: Path to the training dataset. Should contain video and chord subdirectories.
         output_dir: Path to save the trained model.
         seed: Random seed.
         batch_size: Batch size.
@@ -57,6 +57,7 @@ def train(
     set_seed(seed)
     os.makedirs(output_dir, exist_ok=True)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    video_dir, chord_dir = os.path.join(data_dir, "video"), os.path.join(data_dir, "chord")
 
     # Setup model, optimizer and loss function
     config = ScenephonyModelConfig()
@@ -67,34 +68,39 @@ def train(
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = torch.nn.MSELoss()
 
-    # Load videos and target audios from data_dir files, then create dataloader
-    videos, audios = [], []
-    for fn in os.listdir(data_dir):
-        fn_no_ext = fn.split('.')[0]
-        video_path = os.path.join(data_dir, fn_no_ext + ".mp4")
-        # audio_path = os.path.join(data_dir, fn_no_ext + ".wav")
+    # Load videos and target chords from data_dir files, then create dataloader
+    videos, chords = [], []   # videos.shape: (N, T, C, H, W), chords.shape: (N, K)
+    for fn in os.listdir(video_dir):
         video = read_video(
-            video_path, 
+            os.path.join(data_dir, fn), 
             start_pts=10, end_pts=20, pts_unit="sec", 
             output_format="TCHW"
         )[0].to(device)
 
-        # Resize video to have frame size of 224x224
         video = preproc_vid(
             video, config.sample_frames, config.hidden_frame_h, config.hidden_frame_w
         )
         videos.append(video)
+    videos = torch.stack(videos)
 
-    dataset = torch.utils.data.TensorDataset(torch.stack(videos), torch.stack(audios))
+    for fn in os.listdir(chord_dir):
+        f = open(os.path.join(data_dir, fn), "r")
+        chord_seq = [chord_to_number(line.strip()[-1]) for line in f.readlines()]
+        chords.append(torch.Tensor(chord_seq))
+    chords = torch.stack(chords)
+
+    print(f"Loaded videos of shape {videos.shape} and chords of shape {chords.shape}.")
+
+    dataset = torch.utils.data.TensorDataset(videos, chords)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # Start training
     for epoch in range(num_epochs):
-        for i, (video, audio) in tqdm(enumerate(dataloader)):
+        for i, (video, chord) in tqdm(enumerate(dataloader)):
             model.train()
             optimizer.zero_grad()
-            pred_audio = model(video)
-            loss = criterion(pred_audio, audio)
+            pred_chord = model(video)
+            loss = criterion(pred_chord, chord)
             loss.backward()
             optimizer.step()
 
@@ -114,7 +120,7 @@ def test(
     """Generate music using the model (trained if checkpoint_path is provided).
 
     Args:
-        data_dir: Path to the test dataset.
+        data_dir: Path to the test dataset. Should contain video and chord subdirectories.
         output_dir: Path to save the generated music.
         seed: Random seed.
         checkpoint_path: Path to the trained model checkpoint.
@@ -131,8 +137,8 @@ def test(
         model.load_state_dict(torch.load(checkpoint_path, map_location=device))
     model.to(device)
 
-    # Load data, inference and save audio files
-    for vid_fn in tqdm(os.listdir(data_dir)):
+    # Load data, inference and save chord files
+    for vid_fn in tqdm(os.listdir(os.path.join(data_dir, "video"))):
         vid_path = os.path.join(data_dir, vid_fn)
         video = read_video(
             vid_path, 
@@ -144,9 +150,12 @@ def test(
         )
         print(video.shape)
 
-        audio = model(video.unsqueeze(0))   # inp_shape: (1, T, C, H, W)
-        audio_path = os.path.join(output_dir, vid_fn.replace('.mp4', '.wav'))
-        torchaudio.save(audio_path, audio.cpu(), model.audio_sample_rate)
+        # Infer chord sequence and write to txt file
+        chord = model(video.unsqueeze(0))   # inp_shape: (1, T, C, H, W), out_shape: (1, K)
+        chord_path = os.path.join(output_dir, vid_fn.replace('.mp4', '.wav'))
+        chord = chord.squeeze().cpu()
+        with open(chord_path, "w") as f:
+            f.write("\n".join([str(int(c)) for c in chord]))
 
 
 def set_seed(seed: int = 42):
